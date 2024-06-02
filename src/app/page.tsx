62846041bfe3e4ea9a852400ed975d8961dba277
Emre Arabaci <emrearabaci16@gmail.com>
2024-06-02 23:07:34 +0300

    Completed homepage design.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,49 @@
+/* Next API */
+import Image from 'next/image';
+import Link from 'next/link';
+
+/* Components */
+import Button from '@/app/_components/Button/Button';
+
 /* Styles */
 import styles from '@/app/page.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>homepage</p>
+      <div className={styles.left}>
+        <h1 className={styles.typingAnimation}>TASKLINE</h1>
+        <p>
+          This app lists the tasks to be performed by the people in the team.
+        </p>
+
+        <p>
+          In the list, completed tasks are on the right side and incomplete
+          tasks are on the left side.
+        </p>
+
+        <p>Each list has a label symbolizing the task to be done.</p>
+
+        <div className={styles.border} />
+
+        <Link href="/taskline" passHref legacyBehavior>
+          <Button type={'button'} onClick={null} content={'explore app'} />
+        </Link>
+      </div>
+
+      <div className={styles.right}>
+        <Image
+          src={'/taskline-bg.svg'}
+          alt={'image'}
+          style={{
+            width: '100%'
+          }}
+          width={300}
+          height={300}
+          quality={80}
+          priority={true}
+        />
+      </div>
     </main>
   );
 }
